a659bb1109ca29793372225ca7fff741f8535b27
Parul Singh <parulsingh@Paruls-MacBook-Air-2.local>
2020-06-27 17:26:12 +0100

    Adding missed file

--- multiply.test.js
+++ multiply.test.js
@@ -1,9 +1,11 @@
 const multiply = require('./multiply');
 
-test('multiply 4 * 2 to equal 8', () => {
-    expect(multiply(4, 2)).toBe(8);
-});
+function testMultiplication (a, b, expected) {
+    expect(multiply(a, b)).toBe(expected)
+}
 
-test('multiply 6 * 16 to equal 96', () => {
-    expect(multiply(6, 16)).toBe(96);
-});
+test('multiply 1, 2 to be 2', () => testMultiplication(1, 2, 2))
+test('multiply 4, 2 to be 8', () => testMultiplication(4, 2, 8))
+test('multiply 6, 16 to be 96', () => testMultiplication(6, 16, 96))
+test('multiply 10, 10 to be 100', () => testMultiplication(10, 10, 100))
+test('multiply 4, 5 to be 20', () => testMultiplication(4, 5, 20))
